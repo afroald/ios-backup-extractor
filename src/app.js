485c3e0b9fdb6aa569559e387a86b3639ff942e7
Roald Hacquebord <roald@hacquebordbuckles.nl>
2017-06-10 22:41:46 +0200

    Use keen ui

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import 'keen-ui/src/bootstrap';
 import Window from './components/Window';
 
 /* eslint-disable no-new */
--- src/components/Window.vue
+++ src/components/Window.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="window">
     <div class="test">Test</div>
+    <ui-button>test</ui-button>
   </div>
 </template>
 
 <script>
+  import UiButton from 'keen-ui/lib/UiButton';
+
   export default {
     name: 'window',
+    components: {
+      UiButton,
+    },
   };
 </script>
 
